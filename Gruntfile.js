ad0db295e6f0bef135e1b60d658dac82b0bb174c
Oleg <oleg1990moskalenko@gmail.com>
2017-05-25 14:27:53 +0300

    play with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,36 @@ module.exports = function(grunt) {
     // так мы можем использовать штуки вроде name и version (pkg.name)
     pkg: grunt.file.readJSON('package.json'),
 
+    // настройка autoprefixer для добавления вендорных префиксов
+    autoprefixer: {
+      build: {
+        files: {
+          'css/style.css': 'css/style.css'
+        },
+      },
+    },
+
+    // компиляция Less в CSS
+   less: {
+     build: {
+       files: {
+         'css/style.css': 'less/style.less'
+       }
+     }
+   },
+
+   // параметры cssmin для минимизации CSS-файлов
+   cssmin: {
+     options: {
+       banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+     },
+     build: {
+       files: {
+         'css/style.min.css': 'css/style.css'
+       }
+     }
+   },
+
     // настройка jshint для валидации JS-файлов
     jshint: {
       options: {
@@ -25,33 +55,13 @@ module.exports = function(grunt) {
      }
    },
 
-   // компиляция Less в CSS
-  less: {
-    build: {
-      files: {
-        'css/style.css': 'less/style.less'
-      }
-    }
-  },
-
-  // параметры cssmin для минимизации CSS-файлов
-  cssmin: {
-    options: {
-      banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
-    },
-    build: {
-      files: {
-        'css/style.min.css': 'css/style.css'
-      }
-    }
-  },
-
   // параметры watch для автообновления
   watch: {
+    files: '*.html',
     // для стилей следим только за CSS и Less-файлами
     // и запускаем less и cssmin: {
     files: ['css/*.css', 'less/*.less'],
-    tasks: ['less', 'cssmin'],
+    tasks: ['less', 'autoprefixer', 'cssmin'],
 
     // для скриптов запускаем jshint и uglify
     scripts: {
@@ -60,21 +70,33 @@ module.exports = function(grunt) {
     }
   },
 
+  express: {
+    all: {
+      options: {
+        bases: 'mishka-adaptive',
+        livereload: true,
+        open: 'http://127.0.0.1:8080/'
+      }
+    }
+  },
+  // clean cache command
+		clean:["cache/*"]
 });
-// ============= // СОЗДАЁМ ЗАДАЧИ ========== //
-grunt.registerTask('default', ['jshint', 'uglify', 'less', 'cssmin' ]);
 // grunt.registerTask('default', 'lint less');
   // ===========================================================================
   // ЗАГРУЗКА ПЛАГИНОВ GRUNT ===================================================
   // ===========================================================================
   // мы можем их загрузить, только если они находятся в нашем package.json
   // убедитесь, что вы запустили npm install, чтобы наше приложение могло их найти
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-reload');
+  grunt.loadNpmTasks('grunt-express');
 
-
+  // ============= // СОЗДАЁМ ЗАДАЧИ ========== //
+  //grunt.registerTask('default', ['less', 'autoprefixer', 'cssmin', 'jshint', 'uglify']);
+  grunt.registerTask('start', ['express', 'watch']);
 };
