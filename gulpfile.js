593c9525d2e80204e16a62a58f29b38097492302
Oleg <oleg1990moskalenko@gmail.com>
2017-05-25 15:27:27 +0300

    create dist/css/style.css use command '--gult less'

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'), //подключаем пакеты Gulp из папки node_modules в наш проект
-    less = require('gulp-less'), //Подключаем Less пакет
+    less = require('gulp-less'); //Подключаем Less пакет
 
-gulp.task('less', function() { // Создаем таск "sass"
+gulp.task('less', function () { // Создаем таск "sass"
   return gulp.src('app/less/style.less') // Берем источник
     .pipe(less()) // Преобразуем less в CSS посредством gulp-less
-    .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+    .pipe(gulp.dest('./dist/css')); // Выгружаем результата в папку app/css
 });
