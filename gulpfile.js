445818e67ed1e7b9cce98e0cdd692e2122af96ec
Oleg <oleg1990moskalenko@gmail.com>
2017-05-25 15:33:43 +0300

    chage template to return gulp.src('app/less/**/*.less')

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'), //подключаем пакеты Gulp из пап
     less = require('gulp-less'); //Подключаем Less пакет
 
 gulp.task('less', function () { // Создаем таск "sass"
-  return gulp.src('app/less/style.less') // Берем источник
+  return gulp.src('app/less/**/*.less') // Берем все Less-файлы из папки less и дочерних, если таковые будут
     .pipe(less()) // Преобразуем less в CSS посредством gulp-less
     .pipe(gulp.dest('./dist/css')); // Выгружаем результата в папку app/css
 });
