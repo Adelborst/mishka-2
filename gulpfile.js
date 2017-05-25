8cb216580430077005e769ff2655a121674abed6
Oleg <oleg1990moskalenko@gmail.com>
2017-05-25 15:19:45 +0300

    Change task name from sass to less in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'), //подключаем пакеты Gulp из папки node_modules в наш проект
-    less = require('gulp-less'); //Подключаем Less пакет
+    less = require('gulp-less'), //Подключаем Less пакет
 
-gulp.task('sass', function() { // Создаем таск "sass"
+gulp.task('less', function() { // Создаем таск "sass"
   return gulp.src('app/less/style.less') // Берем источник
     .pipe(less()) // Преобразуем less в CSS посредством gulp-less
     .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
