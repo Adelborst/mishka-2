e8923e5b98ac0b3a845ebec1490af5268b4d1dfc
Oleg <oleg1990moskalenko@gmail.com>
2017-05-25 15:53:13 +0300

    create task 'browser-sync' in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp'), //подключаем пакеты Gulp из папки node_modules в наш проект
     less = require('gulp-less'), //Подключаем Less пакет
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'), //Подключаем watch пакет
+    browserSync = require('browser-sync'); //Подключаем browser-sync пакет
 
 gulp.task('less', function () { // Создаем таск "sass"
   return gulp.src('app/less/**/*.less') // Берем все Less-файлы из папки less и дочерних, если таковые будут
     .pipe(less()) // Преобразуем less в CSS посредством gulp-less
-    .pipe(gulp.dest('./dist/css')); // Выгружаем результата в папку app/css
+    .pipe(gulp.dest('./app/css')); // Выгружаем результата в папку app/css
+    .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['browser-sync', 'less'] function () {
   gulp.watch('app/less/**/*less', ['less']); // Наблюдение за less-файлами
-    //add watch for Наблюдениz за другими типами файлов
+    //add watch for Наблюдения за другими типами файлов
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+  browserSync({ // Выполняем browser Sync
+    server: { // Определяем параметры сервера
+      baseDir: 'app' // Директория для сервера - app
+    },
+    notify: false // Отключаем уведомления
+  });
 });
