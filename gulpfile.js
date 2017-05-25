642c229a6e86e64c7780f8d0ec9b98b828fc6b54
Oleg <oleg1990moskalenko@gmail.com>
2017-05-25 16:01:45 +0300

    result work task 'watch' & create ap/css/style.css

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ var gulp = require('gulp'), //подключаем пакеты Gulp из пап
 gulp.task('less', function () { // Создаем таск "sass"
   return gulp.src('app/less/**/*.less') // Берем все Less-файлы из папки less и дочерних, если таковые будут
     .pipe(less()) // Преобразуем less в CSS посредством gulp-less
-    .pipe(gulp.dest('./app/css')); // Выгружаем результата в папку app/css
-    .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+    .pipe(gulp.dest('./app/css')) // Выгружаем результата в папку app/css
+    .pipe(browserSync.reload({stream: true})); // Обновляем CSS на странице при изменении
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
@@ -19,7 +19,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
   });
 });
 
-gulp.task('watch', ['browser-sync', 'less'] function () {
+gulp.task('watch', ['browser-sync', 'less'], function () {
   gulp.watch('app/less/**/*less', ['less']); // Наблюдение за less-файлами
     //add watch for Наблюдения за другими типами файлов
 });
