ea49440a0a51c45f7449df800157d9828fc0e5d0
Oleg Moskalenko <oleg1990moskalenko@gmail.com>
2017-06-14 23:43:15 +0300

    added sleder for reviews-item

--- app/js/main.js
+++ app/js/main.js
@@ -14,7 +14,45 @@ navToggle.addEventListener('click', function() {
 });
 
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider-item");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length} ;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
 // var slider = document.querySelector(".slider");
-// var sliderItem = slider.querySelector(".slider-item");
-// var slide_left = slider.getElementById(".reviews__btn--prev");
-// var slide_right = slider.getElementById(".reviews__btn--right");
+// // var sliderItem = slider.querySelector(".slider-item");
+// var slide_left = slider.querySelector(".reviews__btn--prev");
+// var slide_right = slider.querySelector(".reviews__btn--right");
+// var slideNum = slider.getElementsByTagName('li').length; // считаем сколько отзывов заложено в слайдер
+// var now = 0; // первым будет выведен отзыв с индексом 0
+// var sliderOffset = [];
+// for (var i = 0; i < slideNum; i++) sliderOffset.push(-i * 104 + 'px'); // создаем массив с вариантами смещения слайдера для каждой картинки
+//   slide_left.onclick = function() {
+//     --now;// уменьшаем индекс картинки
+//     if(now < 0) now = slideNum - 1; // если залезли в отрицательную область, делаем текущей последнюю картинку
+//       slider.style.left = sliderOffset[now]; //смещаем слайдер в соответствии с текущей картинкой
+//     }
+//   slide_right.onclick = function() {
+//     ++now; // увеличиваем индекс картинки
+//     if(now > slideNum - 1) now = 0; // если индекс больше, чем может быть, делаем текущей первую картинку
+//       slider.style.left = sliderOffset[now]; // см. выше
+//     }
